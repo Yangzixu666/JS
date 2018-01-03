9aae191b028201d8486fe3981f11cc416672b8e7
杨子旭 <578156479@qq.com>
2018-01-03 15:00:05 +0800

    Add files via upload

---
+++ RMGS/soft.js
@@ -0,0 +1,26 @@
+window.onload=function()
+{
+	var listNodes=document.getElementsByTagName("li");
+	for(var i=0;i<listNodes.length;i++)
+	{
+		listNodes[i].onmouseover=function(){
+			this.className="Show";
+		}
+		listNodes[i].onmouseout=function(){
+			this.className="";
+		}
+	}
+	var flag=1;
+	var interval=setInterval(Change,2000);
+	function Change(){
+		if(flag<=4)
+		{
+			var temp=document.getElementById("imgroll");
+			temp.src="image/"+flag+".jfif";
+			flag++;
+		}
+		else
+			flag=1;
+	}
+
+}
